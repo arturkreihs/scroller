d1f8e45fefc0ec54ad0fbfdfd12e22e21d9f59d0
Artur Kreihs <akreihs@ascs.pl>
2023-06-06 15:12:48 +0200

    threading removed

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use std::io::Write;
 use std::io::stdin;
-use std::thread;
 use termion::{
     clear,
     cursor,
@@ -8,24 +7,16 @@ use termion::{
     event::Key,
     raw::IntoRawMode,
 };
-use std::sync::{
-    Arc,
-    mpsc,
-};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ScrollerError {
     #[error(transparent)]
     IO(#[from] std::io::Error),
-
-    #[error(transparent)]
-    Sender(#[from] std::sync::mpsc::SendError<std::string::String>)
 }
 
 pub struct Scroller {
-    screen: Arc<termion::raw::RawTerminal<std::io::Stdout>>,
-    sender: mpsc::Sender<String>,
+    screen: termion::raw::RawTerminal<std::io::Stdout>,
     rows: u16,
 }
 
@@ -37,7 +28,7 @@ impl Default for Scroller {
 
 impl Scroller {
     pub fn new() -> Result<Self, ScrollerError> {
-        let screen = Arc::new(std::io::stdout().into_raw_mode()?);
+        let screen = std::io::stdout().into_raw_mode()?;
         let (_, rows) = termion::terminal_size()?;
         
         // prepare screen
@@ -48,48 +39,30 @@ impl Scroller {
             screen.flush()?;
         }
 
-        let (tx, rx) = mpsc::channel::<String>();
-
-        // start printing
-        {
-            let screen = Arc::clone(&screen);
-            thread::spawn(move || {
-                loop {
-                    // wait for text to arrive
-                    let line = rx.recv().unwrap();
-
-                    // take stdout here
-                    let mut screen = screen.lock();
-
-                    // save current position
-                    write!(screen, "{}", termion::cursor::Save).unwrap();
-
-                    // scroll up
-                    write!(screen, "{}", termion::scroll::Up(1)).unwrap();
-
-                    // go to bottom line - 1
-                    write!(screen, "{}", cursor::Goto(1, rows - 1)).unwrap();
-
-                    // write text to that line
-                    write!(screen, "{}", line).unwrap();
-                    
-                    // restore position
-                    write!(screen, "{}", termion::cursor::Restore).unwrap();
-
-                    screen.flush().unwrap();
-                }
-            });
-        }
-
         Ok(Scroller {
             screen,
-            sender: tx,
             rows,
         })
     }
 
     pub fn write(&mut self, line: &str) -> Result<(), ScrollerError> {
-        self.sender.send(line.into())?;
+        let mut screen = self.screen.lock();
+        // save current position
+        write!(screen, "{}", termion::cursor::Save).unwrap();
+
+        // scroll up
+        write!(screen, "{}", termion::scroll::Up(1)).unwrap();
+
+        // go to bottom line - 1
+        write!(screen, "{}", cursor::Goto(1, self.rows - 1)).unwrap();
+
+        // write text to that line
+        write!(screen, "{}", line).unwrap();
+        
+        // restore position
+        write!(screen, "{}", termion::cursor::Restore).unwrap();
+
+        screen.flush().unwrap();
         Ok(())
     }
 
